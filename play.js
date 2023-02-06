f84ff90b96bbee546badc85923cbf9cd33ecdffa
Seokju Hong <iamseokjuhong@gmail.com>
2023-02-06 23:06:37 +0900

    13. let & const

--- play.js
+++ play.js
@@ -1,6 +1,8 @@
-var name = 'Max';
-var age = 29;
-var hasHobbies = true;
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
 
 function summarizeUser(userName, userAge, userHasHobby) {
     return 'Name is ' + userName + ', age is ' + userAge + ' and user has hobbies: ' + userHasHobby;
