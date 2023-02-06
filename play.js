0b195b1174eeec913db9faab79c2858a838ba416
Seokju Hong <iamseokjuhong@gmail.com>
2023-02-06 23:34:11 +0900

    15. 개체, 속성 및 메서드 작업

--- play.js
+++ play.js
@@ -1,18 +1,11 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
+const person = {
+    name: 'Max',
+    age: 29,
 
-age = 30;
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
 
-const summarizeUser = (userName, userAge, userHasHobby) => {
-    return 'Name is ' + userName + ', age is ' + userAge + ' and user has hobbies: ' + userHasHobby;
-}
-
-const add = (a, b) => a + b;
-const addOne = a => a + 1;
-const justPrint = () => console.log("just do it");
-
-console.log(add(1, 2));
-console.log(addOne(2));
-justPrint();
-console.log(summarizeUser(name, age, hasHobbies));
+console.log(person)
+person.greet();
