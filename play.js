b1a538442b803891a3f39725ab3c2cb2cfa80346
Seokju Hong <iamseokjuhong@gmail.com>
2023-02-06 23:18:10 +0900

    14. 화살표 함수의 이해

--- play.js
+++ play.js
@@ -4,8 +4,15 @@ const hasHobbies = true;
 
 age = 30;
 
-function summarizeUser(userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
     return 'Name is ' + userName + ', age is ' + userAge + ' and user has hobbies: ' + userHasHobby;
 }
 
+const add = (a, b) => a + b;
+const addOne = a => a + 1;
+const justPrint = () => console.log("just do it");
+
+console.log(add(1, 2));
+console.log(addOne(2));
+justPrint();
 console.log(summarizeUser(name, age, hasHobbies));
