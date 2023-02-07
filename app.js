ad8223076fe624cd5897faa3c116680f97ea1b95
somefood <iamseokjuhong@gmail.com>
2023-02-07 17:51:00 +0900

    59. Express.js 설치

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http'); // 변수명은 자유롭게 지어도 됨, 경로가 없으면 글로벌 모듈 갖고옴
-const routes = require('./routes');
 
-console.log(routes.someText);
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express()
+
+const server = http.createServer(app);
 
 server.listen(3000);
