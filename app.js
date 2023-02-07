4076bee01da9deb35faf9eb8cf21af2e18c73f7f
somefood <iamseokjuhong@gmail.com>
2023-02-07 18:43:58 +0900

    64. 수신 요청 분석

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
     console.log('This always runs!');
@@ -9,7 +12,12 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
   console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
