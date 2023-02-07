f6f78ac174dc0dfdb0e6c0ddb39af1287612f2e3
Hong SeokJu <10017011@wjtb.net>
2023-02-07 14:09:28 +0900

    32. 라우터 요청

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http'); // 변수명은 자유롭게 지어도 됨, 경로가 없으면 글로벌 모듈 갖고옴
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
 
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>First Page</title></head>');
