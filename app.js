88c0fc7e551440b1eea4b275edbe739436a9d4c0
Hong SeokJu <10017011@wjtb.net>
2023-02-07 14:03:52 +0900

    30. 응답 전송

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require('http'); // 변수명은 자유롭게 지어도 됨, 경로
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head>');
+    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
