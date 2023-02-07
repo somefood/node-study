a3e1b2246710e2d86f41c5e3518e568b79938424
somefood <iamseokjuhong@gmail.com>
2023-02-07 18:20:07 +0900

    63. 다른 라우트 사용법

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const http = require('http'); // 변수명은 자유롭게 지어도 됨, 경로가 없으면 글로벌 모듈 갖고옴
-
 const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
