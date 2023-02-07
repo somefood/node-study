f90334d6b78f3d62e70232f4a6ba78984b63d80b
Seokju Hong <iamseokjuhong@gmail.com>
2023-02-07 21:19:09 +0900

    66. Express 라우터 사용

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
