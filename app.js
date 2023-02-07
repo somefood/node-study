552bc70ef19c87abc66bedef7112c8d9e25682e0
Hong SeokJu <10017011@wjtb.net>
2023-02-07 13:21:11 +0900

    26. Node 서버 생성

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http'); // 변수명은 자유롭게 지어도 됨, 경로가 없으면 글로벌 모듈 갖고옴
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+
+server.listen(3000);
--- first-app.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
-
-// console.log('Hello from Node.js');
