47005d628ae4e9b7720dd40803b7191378d31f1c
Hong SeokJu <10017011@wjtb.net>
2023-02-07 13:38:51 +0900

    29: 요청의 이해

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const http = require('http'); // 변수명은 자유롭게 지어도 됨, 경로가 없으면 글로벌 모듈 갖고옴
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);
 });
 
 server.listen(3000);
