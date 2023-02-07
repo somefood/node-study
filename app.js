4026b0aa7848f08efc2f43a82dd52032e78afa17
Seokju Hong <iamseokjuhong@gmail.com>
2023-02-07 21:34:01 +0900

    67. 404 오류 페이지 추가

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
