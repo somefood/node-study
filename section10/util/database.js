1b8350f6e02ee5167b5abb446e55a8f9600e23ac
somefood <iamseokjuhong@gmail.com>
2023-02-08 13:11:13 +0900

    151. 데이터베이스에 연결

--- section10/util/database.js
+++ section10/util/database.js
@@ -1,10 +1,20 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: '172.31.128.66',
-    user: 'root',
-    database: 'seokju',
-    password: 'tjrwn12'
+// const pool = mysql.createPool({
+//     host: '172.31.128.66',
+//     user: 'root',
+//     database: 'seokju',
+//     password: 'tjrwn12'
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('seokju', 'root', 'tjrwn12', {
+    dialect: 'mysql',
+    host: '172.31.128.66'
 });
 
-module.exports = pool.promise();
+// 커넥션 풀 불러오기
+module.exports = sequelize;
