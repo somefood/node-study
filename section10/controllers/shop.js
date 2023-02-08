96c07c3e7aeb305ef274e696981e889f65c860d4
somefood <iamseokjuhong@gmail.com>
2023-02-08 13:55:34 +0900

    157. where 조건으로 단일 제품 얻기
    findByPk로 단일 값을 찾을 수 있고,
    findAll에서는 where를 활용해 필터링 걸 수 있음

--- section10/controllers/shop.js
+++ section10/controllers/shop.js
@@ -17,11 +17,23 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
+  //     console.log(products);
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+
+  Product.findByPk(prodId)
+    .then(product => {
+      console.log(product);
       res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product[0].title,
+        product: product,
+        pageTitle: product.title,
         path: '/products'
       });
     })
