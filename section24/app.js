5f0473bb7b10d231755e2092e15fefd7eb82bf2b
somefood <iamseokjuhong@gmail.com>
2023-02-09 10:40:01 +0900

    365. REST API, 클라이언트 및 CORS 오류

--- section24/app.js
+++ section24/app.js
@@ -8,6 +8,13 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded, 폼 데이터 받을 때
 app.use(bodyParser.json()); // application/json 파싱
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
