91eb0d7ff7f5e601a51e2e2a2d9333dc4e77a63d
somefood <iamseokjuhong@gmail.com>
2023-02-09 09:54:50 +0900

    364. 요청 및 응답 보내기 및 Postman 작업

--- section24/app.js
+++ section24/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded, 폼 데이터 받을 때
+app.use(bodyParser.json()); // application/json 파싱
 
 app.use('/feed', feedRoutes);
 
