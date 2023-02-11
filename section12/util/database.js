6cf15eca8843654378b4ad250d356ae503dae3d4
Seokju Hong <iamseokjuhong@gmail.com>
2023-02-11 23:57:46 +0900

    180. MongoDB 드라이버 설치하기

--- section12/util/database.js
+++ section12/util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(`mongodb+srv://node:pass@cluster0.tyg8s6f.mongodb.net/?retryWrites=true&w=majority`)
+    .then(client => {
+      console.log('Connected');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
